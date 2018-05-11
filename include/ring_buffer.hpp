ce5b82819d65101c6f4fdf82c4c0362dcd89e3c8
Gregory Meyer <gregjm@umich.edu>
2018-05-11 17:27:12 -0400

    partial impl of RingBuffer

--- include/ring_buffer.hpp
+++ include/ring_buffer.hpp
@@ -9,23 +9,31 @@
 #include <iterator>
 #include <memory>
 #include <type_traits>
+#include <utility>
 #include <vector>
 
 namespace gregjm {
 namespace containers {
 
+template <typename T, typename Allocator = std::allocator<T>>
+class RingBuffer;
+
+template <typename T>
 class RingBufferIterator;
 
+template <typename T>
 class RingBufferConstIterator;
 
-template <typename T, typename Allocator = std::allocator<T>>
+template <typename T, typename Allocator>
 class RingBuffer {
+    using TraitsT = std::allocator_traits<Allocator>;
+
 public:
     using value_type = T;
     using reference = T&;
     using const_reference = const T&;
-    using iterator = RingBufferIterator;
-    using const_iterator = RingBufferConstIterator;
+    using iterator = RingBufferIterator<T>;
+    using const_iterator = RingBufferConstIterator<T>;
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
     using allocator_type = Allocator;
@@ -61,7 +69,15 @@ public:
     RingBuffer(std::initializer_list<value_type> list,
                const allocator_type &allocator = allocator_type{ });
 
-    ~RingBuffer();
+    ~RingBuffer() {
+        clear();
+
+        if (data_) {
+            TraitsT::deallocate(allocator_, data_);
+            data_ = nullptr;
+            capacity_ = 0;
+        }
+    }
 
     RingBuffer& operator=(const RingBuffer &other);
 
@@ -100,41 +116,124 @@ public:
 
     const value_type* data() const noexcept;
 
-    iterator begin() noexcept;
+    iterator begin() noexcept {
+        return { data(), size(), front_index() };
+    }
 
-    const_iterator begin() const noexcept;
+    const_iterator begin() const noexcept {
+        return cbegin();
+    }
 
-    const_iterator cbegin() const noexcept;
+    const_iterator cbegin() const noexcept {
+        return { data(), size(), front_index() };
+    }
 
-    iterator end() noexcept;
+    iterator end() noexcept {
+        return { data(), size(), end_index() };
+    }
 
-    const_iterator end() const noexcept;
+    const_iterator end() const noexcept {
+        return cend();
+    }
 
-    const_iterator cend() const noexcept;
+    const_iterator cend() const noexcept {
+        return { data(), size(), end_index() };
+    }
 
-    reverse_iterator rbegin() noexcept;
+    reverse_iterator rbegin() noexcept {
+        return { end() };
+    }
 
-    const_reverse_iterator rbegin() const noexcept;
+    const_reverse_iterator rbegin() const noexcept {
+        return crbegin();
+    }
 
-    const_reverse_iterator crbegin() const noexcept;
+    const_reverse_iterator crbegin() const noexcept {
+        return { cend() };
+    }
 
-    reverse_iterator rend() noexcept;
+    reverse_iterator rend() noexcept {
+        return { begin() };
+    }
 
-    const_reverse_iterator rend() const noexcept;
+    const_reverse_iterator rend() const noexcept {
+        return crend();
+    }
 
-    const_reverse_iterator crend() const noexcept;
+    const_reverse_iterator crend() const noexcept {
+        return { cbegin() };
+    }
 
-    bool empty() const noexcept;
+    bool empty() const noexcept {
+        return size() == 0;
+    }
 
-    size_type size() const noexcept;
+    size_type size() const noexcept {
+        return size_;
+    }
 
-    size_type max_size() const noexcept;
+    size_type max_size() const noexcept {
+        return static_cast<size_type>(TraitsT::max_size(allocator_));
+    }
 
-    void reserve(size_type new_capacity);
+    void reserve(size_type new_capacity) {
+        if (new_capacity <= capacity_) {
+            return;
+        }
 
-    void shrink_to_fit();
+        pointer new_data = TraitsT::allocate(allocator_, new_capacity);
 
-    void clear() noexcept;
+        if constexpr (std::is_nothrow_move_constructible_v<value_type>) {
+            pointer new_element = new_data;
+
+            for (auto &element : *this) {
+                TraitsT::construct(allocator_, new_element,
+                                   std::move(element));
+                ++new_element;
+            }
+        } else { // copying may throw, but it won't mutate our old buffer
+            pointer new_element = new_data;
+
+            try {
+                for (const auto &element : *this) {
+                    TraitsT::construct(allocator_, new_element,
+                                       element);
+                    ++new_element;
+                }
+            } catch (...) { // only the constructor might throw here
+                for (pointer element = 0; element < new_element; ++element) {
+                    TraitsT::destroy(allocator_, element);
+                }
+
+                TraitsT::deallocate(allocator_, new_data, new_capacity);
+                throw;
+            }
+        }
+
+        clear(); // destruct all elements
+
+        using std::swap;
+        swap(data_, new_data); // allocator pointers must be nothrow
+
+        TraitsT::deallocate(allocator_, new_data, new_capacity);
+
+        capacity_ = new_capacity;
+    }
+
+    void shrink_to_fit() {
+        if (capacity_ == size_) {
+            return;
+        }
+    }
+
+    void clear() noexcept {
+        for (auto &element : *this) {
+            TraitsT::destroy(allocator_, { &element });
+        }
+
+        size_ = 0;
+        begin_ = 0;
+    }
 
     iterator insert(const_iterator position, const value_type &value);
 
@@ -189,10 +288,26 @@ public:
              || std::allocator_traits<allocator_type>::is_always_equal_v);
 
 private:
+    size_type front_index() const noexcept {
+        return begin_;
+    }
+
+    size_type back_index() const noexcept {
+        return (begin_ + size_) % capacity_;
+    }
+
+    size_type end_index() const noexcept {
+        return (begin_ + size_ + 1) % capacity_;
+    }
+
+    size_type rend_index() const noexcept {
+        return (begin_ - 1) % capacity_;
+    }
+
     pointer data_ = nullptr;
-    size_type size_ = 0;
+    size_type size_ = 0; // invariant: size <= capacity_
     size_type capacity_ = 0;
-    size_type begin_ = 0;
+    size_type begin_ = 0; // invariant: begin_ < capacity_
     allocator_type allocator_{ };
 };
 
