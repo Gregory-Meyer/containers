e803c284707ca909c62f0d6810243d3edf4f7359
Gregory Meyer <gregjm@umich.edu>
2018-05-20 21:13:12 -0400

    rename Bucket to TombstoneBucket, impl LinearProbe

--- include/set/linear_probe.hpp
+++ include/set/linear_probe.hpp
@@ -1,9 +1,11 @@
 #ifndef GREGJM_CONTAINERS_SET_LINEAR_PROBE_HPP
 #define GREGJM_CONTAINERS_SET_LINEAR_PROBE_HPP
 
-#include "set/bucket.hpp"
+#include "set/tombstone_bucket.hpp"
 
+#include <algorithm>
 #include <functional>
+#include <iterator>
 #include <type_traits>
 
 #include <gsl/gsl>
@@ -14,31 +16,81 @@ namespace gregjm::containers::set {
 template <typename Value>
 class LinearProbe {
 public:
-    using BucketSpanT = gsl::span<Bucket<Value>>;
-    using BucketSpanIterT = typename BucketSpanT::iterator;
+    using BucketT = TombstoneBucket<Value>;
+    using SpanT = gsl::span<BucketT>;
 
-    static BucketSpanIterT insert(const BucketSpanT buckets, Value &&value,
-                                  const std::size_t hash)
-    noexcept(std::is_nothrow_move_constructible_v<Value>) {
-        const std::size_t start_index = hash % buckets.size();
-        const auto start_iter = buckets.begin() + start_index;
+    explicit LinearProbe(const SpanT buckets) noexcept
+    : buckets_{ buckets } { }
 
-        const auto insert_iter = [&buckets, start_iter] {
-            const auto first =
-                std::find_if(start_iter, buckets.end(), &valueless);
+    const SpanT& buckets() const noexcept {
+        return buckets_;
+    }
 
-            if (first == buckets.end()) {
-                const auto second = std::find_if(buckets.begin(), start_iter,
-                                                 &valueless);
+    template <
+        typename Key, typename Equal,
+        std::enable_if_t<std::is_invocable_v<Equal, Key, const Value&>
+                         || std::is_invocable_v<Equal, const Value&, Key>,
+                         int> = 0
+    >
+    typename SpanT::const_iterator
+    find(Key &&key, const std::size_t hash, Equal &&equal) const noexcept {
+        if (buckets_.empty()) {
+            return buckets_.cend();
+        }
 
-                return second == start_iter ? buckets.end() : second;
+        const std::size_t start_index = hash % num_buckets();
+        const auto start_iter =
+            buckets_.cbegin() + static_cast<SpanDiffT>(start_index);
+
+        const auto is_equal_or_empty = [&key, &equal](const BucketT &bucket) {
+            if (bucket.is_empty()) {
+                return true;
+            } else if (!bucket.has_value()) {
+                return false;
+            }
+
+            const auto &value = bucket.get_value().value().get();
+
+            if constexpr (std::is_invocable_v<Equal, Key, const Value&>) {
+                return std::invoke(std::forward<Equal>(equal),
+                                   std::forward<Key>(key), value);
+            } else {
+                return std::invoke(std::forward<Equal>(equal), value,
+                                   std::forward<Key>(key));
+            }
+        };
+
+        const auto first = std::find_if(start_iter, buckets_.cend(),
+                                        is_equal_or_empty);
+
+        if (first != buckets_.cend()) {
+            if (first->is_empty()) {
+                return buckets_.cend();
             }
 
             return first;
-        }();
+        }
 
-        if (insert_iter == buckets.end()) {
-            return nullptr;
+        const auto second = std::find_if(buckets_.cbegin(), start_iter,
+                                         is_equal_or_empty);
+
+        if (second == start_iter || second->is_empty()) {
+            return buckets_.cend();
+        }
+
+        return second;
+    }
+
+    typename SpanT::iterator
+    insert(Value &&value, const std::size_t hash) const {
+        if (buckets_.empty()) {
+            return buckets_.end();
+        }
+
+        const auto insert_iter = to_iter(find_insert(hash));
+
+        if (insert_iter == buckets_.end()) {
+            return buckets_.end();
         }
 
         insert_iter->emplace(std::move(value));
@@ -48,41 +100,95 @@ public:
     template <
         typename Key, typename Equal,
         std::enable_if_t<
-            std::is_invocable_v<const Equal&, const Key&, const Value&>
-            || std::is_invocable_v<const Equal&, const Value&, const Key&>,
+            std::is_invocable_v<Equal, Key, const Value&>
+            || std::is_invocable_v<Equal, const Value&, Key>,
             int
         > = 0
     >
-    static BucketSpanIterT erase(const BucketSpanT buckets, const Key &key,
-                                 const std::size_t hash, const Equal &equal) {
-        const std::size_t start_index = hash % buckets.size();
-        const auto start_iter = buckets.begin() + start_index;
+    typename SpanT::iterator
+    erase(Key &&key, const std::size_t hash, Equal &&equal) const {
+        if (buckets_.empty()) {
+            return buckets_.end();
+        }
 
-        const auto delete_iter = [&buckets, start_iter, &key, &equal] {
-            const auto is_equal = [&key, &equal](const Bucket<Value> &bucket) {
-                if (!bucket.has_value()) {
-                    return false;
-                }
+        const auto delete_const_iter = find(std::forward<Key>(key), hash,
+                                            std::forward<Equal>(equal));
+        const auto delete_iter = to_iter(delete_const_iter);
 
-                if constexpr (
-                    std::is_invocable_v<const Equal&, const Key&, const Value&>
-                ) {
-                    return std::invoke(equal, key, bucket.get_value().value());
-                } else {
-                    return std::invoke(equal, bucket.get_value().value(), key);
-                }
-            }();
+        if (delete_iter == buckets_.end()) {
+            return buckets_.end();
+        }
 
-            const auto first =
-                std::find_if(start_iter, buckets.end(), is_equal);
-        }();
+        delete_iter->set_deleted();
+        return delete_iter;
+    }
+
+    template <typename Hasher>
+    SpanT move_to(Hasher &&hasher, SpanT new_buckets) {
+        LinearProbe new_policy(new_buckets);
+
+        for (auto &bucket : buckets_) {
+            if (!bucket.has_value()) {
+                continue;
+            }
+
+            Value &value = bucket.get_value().value();
+            const std::size_t hash =
+                std::invoke(std::forward<Hasher>(hasher), value);
+
+            new_policy.insert(std::move(value), hash);
+            bucket.set_deleted();
+        }
+
+        {
+            using std::swap;
+            swap(buckets_, new_buckets);
+        }
+
+        return new_buckets;
     }
 
 private:
-    static bool valueless(const Bucket<Value> &bucket) noexcept {
-        return !bucket.has_value();
+    using SpanDiffT = typename SpanT::iterator::difference_type;
+
+    typename SpanT::iterator
+    to_iter(const typename SpanT::const_iterator iter) const noexcept {
+        const auto index = std::distance(buckets_.cbegin(), iter);
+
+        return buckets_.begin() + index;
     }
 
+    typename SpanT::const_iterator
+    find_insert(const std::size_t hash) const noexcept {
+        const std::size_t start_index = hash % num_buckets();
+        const auto start_iter =
+            buckets_.cbegin() + static_cast<SpanDiffT>(start_index);
+
+        const auto is_valueless =
+            [](const BucketT &bucket) { return !bucket.has_value(); };
+
+        const auto first = std::find_if(start_iter, buckets_.cend(),
+                                        is_valueless);
+
+        if (first != buckets_.cend()) {
+            return first;
+        }
+
+        const auto second = std::find_if(buckets_.cbegin(), start_iter,
+                                         is_valueless);
+
+        if (second == start_iter) {
+            return buckets_.cend();
+        }
+
+        return second;
+    }
+
+    std::size_t num_buckets() const noexcept {
+        return static_cast<std::size_t>(buckets_.size());
+    }
+
+    SpanT buckets_;
 };
     
 } // namespace gregjm::containers::set
---
+++ include/set/tombstone_bucket.hpp
@@ -0,0 +1,87 @@
+#ifndef GREGJM_CONTAINERS_SET_TOMBSTONE_BUCKET_HPP
+#define GREGJM_CONTAINERS_SET_TOMBSTONE_BUCKET_HPP
+
+#include <functional>
+#include <optional>
+#include <utility>
+#include <variant>
+
+namespace gregjm::containers::set {
+
+template <typename T>
+class TombstoneBucket {
+public:
+    using ValueT = T;
+
+    TombstoneBucket() noexcept = default;
+
+    template <typename ...As,
+              std::enable_if_t<std::is_constructible_v<T, As...>, int> = 0>
+    TombstoneBucket(As &&...args)
+    noexcept(std::is_nothrow_constructible_v<T, As...>)
+    : data_{ std::in_place_type<T>, std::forward<As>(args)... } { }
+
+    bool is_empty() const noexcept {
+        return std::holds_alternative<EmptyBucket>(data_);
+    }
+
+    bool is_deleted() const noexcept {
+        return std::holds_alternative<DeletedBucket>(data_);
+    }
+
+    bool has_value() const noexcept {
+        return std::holds_alternative<T>(data_);
+    }
+
+    T& unwrap() {
+        return std::get<T>(data_);
+    }
+
+    const T& unwrap() const {
+        return std::get<T>(data_);
+    }
+
+    template <typename ...As,
+              std::enable_if_t<std::is_constructible_v<T, As...>, int> = 0>
+    void emplace(As &&...args)
+    noexcept(std::is_nothrow_constructible_v<T, As...>) {
+        if constexpr (std::is_nothrow_constructible_v<T, As...>) {
+            data_.template emplace<T>(std::forward<As>(args)...);
+        } else {
+            try {
+                data_.template emplace<T>(std::forward<As>(args)...);
+            } catch (...) {
+                data_.template emplace<EmptyBucket>();
+                
+                throw;
+            }
+        }
+    }
+
+    void set_empty() noexcept {
+        if (is_empty()) {
+            return;
+        }
+
+        data_.template emplace<EmptyBucket>();
+    }
+
+    void set_deleted() noexcept {
+        if (is_deleted()) {
+            return;
+        }
+
+        data_.template emplace<DeletedBucket>();
+    }
+
+private:
+    class EmptyBucket { };
+
+    class DeletedBucket { }; // tombstone
+
+    std::variant<EmptyBucket, DeletedBucket, T> data_ = EmptyBucket{ }; 
+};
+
+} // namespace gregjm::containers::set
+
+#endif
