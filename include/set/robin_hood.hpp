855597dec4fedd44cba52c74209a926aa1840449
Gregory Meyer <gregjm@umich.edu>
2018-05-24 02:43:14 -0400

    impl RobinHoodIterator and a few member fns

--- include/set/robin_hood.hpp
+++ include/set/robin_hood.hpp
@@ -5,6 +5,9 @@
 #include "utility.hpp"
 
 #include <cstddef>
+#include <optional>
+#include <type_traits>
+#include <utility>
 
 #include <gsl/gsl>
 
@@ -17,6 +20,66 @@ template <typename T>
 class RobinHoodIterator {
 public:
     friend RobinHood<T>;
+
+    using value_type = T;
+    using difference_type = std::ptrdiff_t;
+    using reference = const T&;
+    using pointer = const T*;
+    using iterator_category = std::forward_iterator_tag;
+
+    RobinHoodIterator() = default;
+
+    [[nodiscard]] reference operator*() const {
+        return current_->unwrap();
+    }
+
+    [[nodiscard]] pointer operator->() const {
+        return std::addressof(**this);
+    }
+
+    RobinHoodIterator& operator++() {
+        if (current_ != end_) {
+            ++current_;
+            validate();
+        }
+
+        return *this;
+    }
+
+    RobinHoodIterator operator++(int) {
+        const auto to_return = *this;
+
+        ++(*this);
+
+        return to_return;
+    }
+
+    [[nodiscard]] friend bool operator==(
+        const RobinHoodIterator &lhs, const RobinHoodIterator &rhs
+    ) noexcept {
+        return lhs.base_ == rhs.base_;
+    }
+
+    [[nodiscard]] friend bool operator!=(
+        const RobinHoodIterator &lhs, const RobinHoodIterator &rhs
+    ) noexcept {
+        return lhs.base_ != rhs.base_;
+    }
+
+private:
+    using BaseT = typename RobinHood<T>::view::iterator;
+
+    RobinHoodIterator(BaseT current, BaseT last) noexcept
+    : current_{ std::move(current) }, end_{ std::move(last) } {
+        validate();
+    }
+
+    void validate() noexcept {
+        for (; current_ != end_ && !current_->has_value(); ++current_) { }
+    }
+
+    BaseT current_;
+    BaseT end_;
 };
 
 template <typename T>
@@ -29,19 +92,35 @@ public:
     using size_type = std::make_unsigned_t<typename view::size_type>;
     using difference_type = std::make_signed_t<size_type>;
 
-    explicit RobinHood(const view buckets) noexcept;
+    explicit RobinHood(const view buckets) noexcept : buckets_{ buckets } { }
 
-    [[nodiscard]] iterator begin() const noexcept;
+    [[nodiscard]] iterator begin() const noexcept {
+        return { buckets_.cbegin(), buckets_.cend() };
+    }
 
-    [[nodiscard]] const_iterator cbegin() const noexcept;
+    [[nodiscard]] const_iterator cbegin() const noexcept {
+        return begin();
+    }
 
-    [[nodiscard]] iterator end() const noexcept;
+    [[nodiscard]] iterator end() const noexcept {
+        return { buckets_.cend(), buckets_.cend() };
+    }
 
-    [[nodiscard]] const_iterator cend() const noexcept;
+    [[nodiscard]] const_iterator cend() const noexcept {
+        return end();
+    }
 
-    [[nodiscard]] size_type num_occupied() const noexcept;
+    [[nodiscard]] size_type num_occupied() const noexcept {
+        return num_occupied_;
+    }
 
-    void clear() noexcept;
+    void clear() noexcept {
+        for (auto &bucket : buckets_) {
+            bucket.set_empty();
+        }
+
+        num_occupied_ = 0;
+    }
 
     template <typename K, typename E,
               std::enable_if_t<IS_BINARY_PREDICATE<E, const T&, K>
@@ -65,8 +144,17 @@ public:
     void swap(RobinHood &other) noexcept;
 
 private:
+    std::pair<size_type, std::optional<size_type>>
+    find_insert_swap_indices(const std::size_t hash) {
+        const auto start_index = static_cast<size_type>(hash % num_buckets());
+    }
+
+    size_type num_buckets() const noexcept {
+        return static_cast<size_type>(buckets_.size());
+    }
+
     view buckets_;
-    size_type num_occupied_;
+    size_type num_occupied_ = 0;
 };
 
 template <typename T>
